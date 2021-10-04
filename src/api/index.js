0651f12406d6d1f3c66732b824ec387f280f55cd
hamid2117 <saim008865@gmail.com>
2021-10-04 16:48:00 +0500

    added for get password

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 // develment api
 
-// export const devApi = 'https://zigza-app.herokuapp.com/api/v1/'
-export const devApi = 'http://localhost:5000/api/v1/'
+export const devApi = 'https://zigza-app.herokuapp.com/api/v1/'
+// export const devApi = 'http://localhost:5000/api/v1/'
 
 //production api
 
